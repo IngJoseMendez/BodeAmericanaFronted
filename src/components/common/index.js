ed30459a0e77d463b8fb09ecb6305936227ceb93
Jose Mendez <josealbertomendez13@gmail.com>
2026-04-14 00:53:27 -0500

    Clean: eliminar carpeta src duplicada (frontend está en client/)

--- src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-export { Button } from './Button';
-export { Input, Select, Textarea, CurrencyInput, NumberInput } from './Input';
-export { Card, CardHeader, CardBody, CardTitle, CardDescription } from './Card';
-export { Badge } from './Badge';
-export { Modal } from './Modal';
-export { ToastProvider, useToast } from './Toast';
